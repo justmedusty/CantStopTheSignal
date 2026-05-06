946497bfddef1a63e941dc59bf5d3266e5842afd
dustyn <dustyn_gibb@hotmail.com>
2026-05-06 04:47:22 -0400

    Added and updated PrivateMessagesDsl.kt and renamed PostContentsDsl.kt

---
+++ src/main/kotlin/cantstopthesignal/database/users/PrivateMessagesDsl.kt
@@ -0,0 +1,157 @@
+package cantstopthesignal.database.users
+
+import cantstopthesignal.cryptography.encryptMessage
+import cantstopthesignal.log.logger
+import com.freedom.cantstopthesignal.database.dsl.table_definitions.Messages
+import com.freedom.cantstopthesignal.database.dsl.table_definitions.Messages.message
+import com.freedom.cantstopthesignal.database.dsl.table_definitions.Messages.receiverId
+import com.freedom.cantstopthesignal.database.dsl.table_definitions.Messages.senderId
+import com.freedom.cantstopthesignal.database.dsl.table_definitions.Messages.timeSent
+import org.jetbrains.exposed.v1.core.SortOrder
+import org.jetbrains.exposed.v1.core.and
+import org.jetbrains.exposed.v1.core.eq
+import org.jetbrains.exposed.v1.jdbc.insert
+import org.jetbrains.exposed.v1.jdbc.select
+import org.jetbrains.exposed.v1.jdbc.selectAll
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+import java.time.LocalDateTime
+
+
+data class Message(
+    val id: Long,
+    val senderUserName: String,
+    val receiverUserName: String,
+    val message: String,
+    val timeSent: LocalDateTime
+)
+
+fun sendMessage(sender: Long, receiver: Long, messageString: String): Long? {
+
+    val publicKey: String?
+    var encryptedMessage: ByteArray? = null
+
+    if (hasAutoEncryptionEnabled(receiver)) {
+        publicKey = getPublicKey(receiver)
+        if (publicKey != null) {
+            encryptedMessage = encryptMessage(publicKey, messageString)
+        } else {
+            encryptedMessage = null
+        }
+
+    }
+
+    return try {
+
+        transaction {
+
+            Messages.insert {
+                it[senderId] = sender
+                it[receiverId] = receiver
+
+                if (encryptedMessage != null) {
+                    //TODO remember this is here and make sure this conversion works properly
+                    it[message] = encryptedMessage.contentToString()
+                } else {
+                    it[message] = messageString
+                }
+
+                it[timeSent] = LocalDateTime.now()
+            }get Messages.id
+
+
+        }
+    } catch (e: Exception) {
+        logger.error { e.message }
+        null
+    }
+
+}
+
+fun getMessagesFromUser(requesterId: Long, requestedId: Long, page: Int, limit: Int): List<Message>? {
+    try {
+        val offsetVal = ((page - 1) * limit).toLong()
+
+        // Get sender and receiver usernames
+        val senderUserNameString = getUserName(requestedId)
+        val receiverUserNameString = getUserName(requesterId)
+
+        return if (senderUserNameString != null && receiverUserNameString != null) {
+
+
+            transaction {
+                Messages.selectAll().where{ (Messages.receiverId eq requesterId) and (Messages.senderId eq requestedId) }
+                    .limit(limit).offset(offsetVal).map {
+                        Message(
+                            id = it[Messages.id],
+                            senderUserName = senderUserNameString,
+                            receiverUserName = receiverUserNameString,
+                            message = it[Messages.message],
+                            timeSent = it[Messages.timeSent]
+                        )
+                    }
+            }
+        } else {
+            null
+        }
+    } catch (e: Exception) {
+        logger.error { e.message }
+        return null //return null on error , not empty list, because empty list can mean more than just error, null can only mean error
+    }
+}
+
+fun getAllMessages(userId: Long, page: Int, limit: Int): List<Message>? {
+    val offsetVal = ((page - 1) * limit).toLong()
+    val receiverUserNameString = getUserName(userId)
+    return if (receiverUserNameString != null) {
+        try {
+            transaction {
+
+                Messages.selectAll().where { (Messages.receiverId eq userId) }.limit(limit).offset(offsetVal).map {
+                    val senderId = it[Messages.senderId]
+                    val senderUserNameString = getUserName(senderId) ?: "Unknown"
+
+                    Message(
+                        id = it[Messages.id],
+                        senderUserName = senderUserNameString,
+                        receiverUserName = receiverUserNameString,
+                        message = it[Messages.message],
+                        timeSent = it[Messages.timeSent]
+                    )
+                }
+            }
+        } catch (e: Exception) {
+            logger.error { e.message }
+            null
+        }
+    } else return null
+}
+
+fun getUsersWhoHaveMessagedYou(userId: Long, page: Int, limit: Int): List<ProfileDataEntry>? {
+    val offsetVal = ((page - 1) * limit).toLong()
+    val usersWithProfileData = mutableMapOf<Long, ProfileDataEntry>()
+
+    try {
+        transaction {
+            val senderIdsByMostRecent = Messages.selectAll().where { Messages.receiverId eq userId }
+                .orderBy(Messages.id, SortOrder.DESC)
+                .limit(limit).offset(offsetVal)
+                .map { it[Messages.senderId] }
+
+
+            senderIdsByMostRecent.forEach { senderId ->
+
+                if (!usersWithProfileData.containsKey(senderId)) {
+                    val senderProfileData = getProfileDataEntry(senderId)
+                    if (senderProfileData != null) {
+                        usersWithProfileData[senderId] = senderProfileData
+                    }
+                }
+            }
+        }
+    } catch (e: Exception) {
+        logger.error { e.message }
+        return null
+    }
+
+    return usersWithProfileData.values.toList()
+}
