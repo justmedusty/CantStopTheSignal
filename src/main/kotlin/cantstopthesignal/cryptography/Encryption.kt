d1d25d6b179014462ab75d879dae44473e88ebc9
dustyn <dustyn_gibb@hotmail.com>
2026-05-27 12:54:15 -0400

    fix some of the pgpainless code to remove deprecated functions, set up group encryption function

--- src/main/kotlin/cantstopthesignal/cryptography/Encryption.kt
+++ src/main/kotlin/cantstopthesignal/cryptography/Encryption.kt
@@ -3,9 +3,11 @@ package cantstopthesignal.cryptography
 import cantstopthesignal.log.logger
 import org.bouncycastle.openpgp.PGPPublicKeyRing
 import org.bouncycastle.openpgp.api.MessageEncryptionMechanism
+import org.bouncycastle.openpgp.api.OpenPGPKey
 import org.bouncycastle.util.io.Streams
 import org.pgpainless.PGPainless
 import org.pgpainless.algorithm.SymmetricKeyAlgorithm
+import org.pgpainless.bouncycastle.extensions.toOpenPGPCertificate
 import org.pgpainless.encryption_signing.EncryptionOptions
 import org.pgpainless.encryption_signing.ProducerOptions
 import java.io.ByteArrayInputStream
@@ -24,14 +26,15 @@ fun encryptMessage(publicKey: String, message: String): ByteArray? {
     return try {
         val outputStream = ByteArrayOutputStream()
 
-        val publicKeyObj: PGPPublicKeyRing = PGPainless.readKeyRing().publicKeyRing(publicKey) ?: return null
+        val publicKey: OpenPGPKey = PGPainless.getInstance().readKey().parseKey(publicKey) ?: return null
         val plaintextInputStream = ByteArrayInputStream(message.toByteArray())
         val api: PGPainless = PGPainless.Companion.getInstance()
 
         val encryptionStream = api.generateMessage().onOutputStream(outputStream).withOptions(
             ProducerOptions.encrypt(
-                EncryptionOptions.get(api).addRecipient(publicKeyObj).overrideEncryptionMechanism(
-                    MessageEncryptionMechanism.librePgp(SymmetricKeyAlgorithm.AES_256.ordinal))
+                EncryptionOptions.get(api).addRecipient(publicKey).overrideEncryptionMechanism(
+                    MessageEncryptionMechanism.librePgp(SymmetricKeyAlgorithm.AES_256.ordinal)
+                )
             ).setAsciiArmor(true)
         )
 
@@ -42,10 +45,40 @@ fun encryptMessage(publicKey: String, message: String): ByteArray? {
         val encryptedMessage = Base64.getEncoder().encodeToString(outputStream.toByteArray())
 
         encryptedMessage.toByteArray()
-    }catch (e: Exception){
+    } catch (e: Exception) {
         logger.error { e.message }
         null
     }
 
+}
+
+fun encryptMessageForMany(publicKeys: List<String>, message: String): ByteArray? {
+    return try {
+        val outputStream = ByteArrayOutputStream()
+        val plaintextInputStream = ByteArrayInputStream(message.toByteArray())
+        val api: PGPainless = PGPainless.Companion.getInstance()
+
+        val encryptionOptions = EncryptionOptions.get(api)
+        for (publicKey in publicKeys) {
+            val publicKey: OpenPGPKey = PGPainless.getInstance().readKey().parseKey(publicKey) ?: continue
+            encryptionOptions.addRecipient(publicKey)
+        }
+        encryptionOptions.overrideEncryptionMechanism(
+            MessageEncryptionMechanism.librePgp(SymmetricKeyAlgorithm.AES_256.ordinal)
+        )
+
+        val encryptionStream = api.generateMessage().onOutputStream(outputStream).withOptions(
+            ProducerOptions.encrypt(encryptionOptions).setAsciiArmor(true)
+        )
+
+        Streams.pipeAll(plaintextInputStream, encryptionStream)
+        encryptionStream.close()
+
+        val encryptedMessage = Base64.getEncoder().encodeToString(outputStream.toByteArray())
+        encryptedMessage.toByteArray()
+    } catch (e: Exception) {
+        logger.error { e.message }
+        null
+    }
 }
 
