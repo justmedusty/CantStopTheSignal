cfdc4fd249e53387abdc9be4017b82bc3c34f3b3
dustyn <dustyn_gibb@hotmail.com>
2026-08-19 07:47:16 +0000

    adding creation of admin user if db is empty , using secrets file like the others.

--- secret_setup.sh
+++ secret_setup.sh
@@ -1,5 +1,9 @@
 mkdir -p secrets
 openssl rand -base64 32 > secrets/postgres_password.txt
 openssl rand -base64 48 > secrets/app_secret_key.txt
+openssl rand -base64 32 > secrets/adminpassword.txt
 
-printf "secrets/\n.env\n*.env\n" >> .gitignore
+
+if [[ -z $(grep 'secrets' .gitignore) ]]; then
+  printf "secrets/\n.env\n*.env\n" >> .gitignore
+fi
--- src/main/kotlin/cantstopthesignal/config/Database.kt
+++ src/main/kotlin/cantstopthesignal/config/Database.kt
@@ -1,10 +1,15 @@
 package cantstopthesignal.config
 
+import cantstopthesignal.database.users.User
+import cantstopthesignal.database.users.createUser
+import cantstopthesignal.security.hashPassword
 import cantstopthesignal.table_definitions.*
 import io.ktor.server.application.*
 import org.jetbrains.exposed.v1.jdbc.Database
 import org.jetbrains.exposed.v1.jdbc.SchemaUtils
+import org.jetbrains.exposed.v1.jdbc.selectAll
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+import java.io.File
 import kotlin.system.exitProcess
 
 
@@ -21,7 +26,7 @@ fun Application.configureDatabase() {
     val dbUser = System.getenv("DATABASE_USER") ?: "postgres"
     val dbPassword = System.getenv("DATABASE_PASSWORD_FILE")
 
-        ?.let { java.io.File(it).readText().trim() }
+        ?.let { File(it).readText().trim() }
         ?: error("DATABASE_PASSWORD_FILE is required")
     val jdbcUrl = "jdbc:postgresql://$dbHost:$dbPort/$dbName"
     try {
@@ -52,4 +57,28 @@ fun Application.configureDatabase() {
             InviteCodes // We'll create the table even if its not used by the person using this software
         )
     }
+
+    transaction {
+        if (Users.selectAll().count().toInt() == 0) {
+            val password = System.getenv("ADMIN_PASSWORD")
+
+                ?.let { File(it).readText().trim() }
+                ?: error("DATABASE_PASSWORD_FILE is required")
+            val user: User = User(
+                userName = "admin",
+                passwordHash = hashPassword(password),
+                publicKey = null, // You Can set this in the web interface
+                isAdmin = true,
+                isModerator = false,
+                isSuspended = false,
+            )
+
+            if (!createUser(user)){
+                error("Cannot create admin user!")
+            }
+
+        }
+    }
+
+
 }
